f8a1c5c04426ef3444d0834b9cf016d87718e62b
Jose Sandoval <j-os-pablo@hotmail.com>
2022-03-18 16:00:35 -0500

    deleted svg

--- main.js
+++ main.js
@@ -8,37 +8,3 @@ document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.navbar__menu--list').classList.toggle('show')
 });
 
-
-// function is_gif_image(i) {
-//     return /^(?!data:).*\.gif/i.test(i.src);
-// }
-
-// function freeze_gif(src) {
-//     i = document.getElementById(src);
-//     var c = document.createElement('canvas');
-//     var w = c.width = i.width;
-//     var h = c.height = i.height;
-//     c.getContext('2d').drawImage(i, 0, 0, w, h);
-//     try {
-//         i.src = c.toDataURL("image/gif"); // if possible, retain all css aspects
-//     } catch (e) { // cross-domain -- mimic original with all its tag attributes
-//         for (var j = 0, a; a = i.attributes[j]; j++)
-//             c.setAttribute(a.name, a.value);
-//         i.parentNode.replaceChild(c, i);
-//     }
-// }
-
-// function unfreeze_gif(id, src) {
-//     i = document.getElementById(id);
-//     i.src = src;
-// }
-
-// document.querySelector('.gif-animated__link').addEventListener('mousemove', () => {
-//     document.querySelector('.gif-animated__link').classList.toggle(unfreeze_gif('rotator', 'img/open-door.gif'))
-// });
-
-// document.querySelector('.gif-animated__link').addEventListener('mouseout', () => {
-//     document.querySelector('.gif-animated__link').classList.toggle(freeze_gif('rotator'))
-// });
-
-
